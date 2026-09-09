1ca9a3df4301aac253e93196c12769f24401d654
Farich Murobic <farichmurobiq11@gmail.com>
2026-09-09 19:55:28 +0700

    chore: initialize portfolio backend

---
+++ src/main/java/com/portfolio/presentation/HealthController.java
@@ -0,0 +1,13 @@
+package com.portfolio.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Portfolio API is running!";
+    }
+}
